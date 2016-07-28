9e75bf1011c2e6187f3c392ed9c6887a183dd473
kuubs0n <jakubkrawczyk2014@gmail.com>
2016-07-28 21:08:34 +0200

    Program v1
    Wyszukiwanie plików, logika wykrywania starych oraz wykrywania
    rozszerzeń plików.

--- ClenaerOldFiles/Form1.Designer.vb
+++ ClenaerOldFiles/Form1.Designer.vb
@@ -22,9 +22,36 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        Me.Application(2)
+
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.MinimumSize = New System.Drawing.Size(450, 450)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(500, 500)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Font = New Font("Arial", 12.0, FontStyle.Regular)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(550, 550)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
 End Class
--- ClenaerOldFiles/Form1.vb
+++ ClenaerOldFiles/Form1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Form1
-
+﻿Imports System
+Imports System.IO
+Imports System.Collections
+Public Class Form1
+    Function GetTime() As String
+        Return CStr(Now)
+    End Function
+    Sub DisplayTime()
+        MsgBox(GetTime)
+    End Sub
+    Sub Application(value As Integer)
+        Dim path As String = "C:\vb"
+        Dim currentMinutesTicks As Integer = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalMinutes
+        Dim directoryInfo As New DirectoryInfo(path)
+        Dim fileList As FileInfo() = directoryInfo.GetFiles()
+        TextBox1.AppendText("Current time is: " & currentMinutesTicks & Environment.NewLine)
+        TextBox1.AppendText("Interval is set on: " & value & Environment.NewLine)
+        For Each item In fileList
+            If (item.Extension.Equals("txt")) Then
+                Dim minutes As Integer = TimeSpan.FromTicks(item.CreationTime.Ticks).TotalMinutes
+                TextBox1.AppendText("Name: " & item.Name & " Data utworzenia: " & minutes & Environment.NewLine)
+                TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & value & Environment.NewLine)
+                If (currentMinutesTicks - minutes >= value) Then
+                    System.IO.File.Delete(path & "\" & item.Name)
+                    TextBox1.AppendText("Deleted: " & path & "\" & item.Name & Environment.NewLine)
+                End If
+            End If
+        Next
+    End Sub
 End Class
