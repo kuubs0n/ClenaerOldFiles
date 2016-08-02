ef9937d997385511fa40016bdb5237518f961683
kuubs0n <jakubkrawczyk2014@gmail.com>
2016-08-02 18:28:13 +0200

    DodanieUstawieńINIT

--- ClenaerOldFiles/Form1.vb
+++ ClenaerOldFiles/Form1.vb
@@ -1,28 +1,56 @@
 ﻿Imports System
 Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text
 Imports System.Collections
 Public Class Form1
+
+    Public Declare Auto Function GetPrivateProfileString Lib "kernel32" (ByVal lpAppName As String,
+            ByVal lpKeyName As String,
+            ByVal lpDefault As String,
+            ByVal lpReturnedString As StringBuilder,
+            ByVal nSize As Integer,
+            ByVal lpFileName As String) As Integer
+
     Sub Work()
 
-        Dim arguments As String() = Environment.GetCommandLineArgs
+        'Dim arguments As String() = Environment.GetCommandLineArgs
+        'Dim haveToMove = False
+        'Dim timeToDelete As Integer = arguments(2)
+        'Dim path As String = arguments(1)
+        'Dim pathToMove As String
+        'Try
+        '    pathToMove = arguments(3)
+        '    haveToMove = True
+        'Catch
+        '    haveToMove = False
+        'End Try
+
         Dim haveToMove = False
-        Dim timeToDelete As Integer = arguments(2)
-        Dim path As String = arguments(1)
-        Dim pathToMove As String
+        Dim timeToDelete As StringBuilder = New StringBuilder(100)
+        Dim pathFrom As StringBuilder = New StringBuilder(100)
+        Dim pathTo As StringBuilder = New StringBuilder(100)
+        Dim startupPath As String = Application.StartupPath()
+        GetPrivateProfileString("SETTINGS", "TIME", "", timeToDelete, timeToDelete.Capacity, startupPath & "\config.ini")
+        GetPrivateProfileString("SETTINGS", "FROM", "", pathFrom, pathFrom.Capacity, startupPath & "\config.ini")
         Try
-            pathToMove = arguments(3)
+            GetPrivateProfileString("SETTINGS", "TO", "", pathTo, pathTo.Capacity, startupPath & "\config.ini")
             haveToMove = True
         Catch
             haveToMove = False
         End Try
+        If (pathTo.Length = 0) Then
+            haveToMove = False
+        End If
+
 
         Dim currentMinutesTicks As Integer = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalMinutes
         Dim fileWriter As New StreamWriter("logs.csv", True)
 
         TextBox1.AppendText("Current time is: " & currentMinutesTicks & Environment.NewLine)
-        TextBox1.AppendText("Interval is set on: " & timeToDelete & Environment.NewLine)
+        TextBox1.AppendText("Interval is set on: " & timeToDelete.ToString & Environment.NewLine)
 
-        Dim subfoldersList As String() = GetSubDirectories(path)
+        Dim subfoldersList As String() = GetSubDirectories(pathFrom.ToString)
 
         For Each item In subfoldersList
             Dim directoryInfo As New DirectoryInfo(item)
@@ -33,9 +61,9 @@ Public Class Form1
                 If (item2.Extension.Equals(".pdf") Or item2.Extension.Equals(".jpeg") Or item2.Extension.Equals(".jpg") Or item2.Extension.Equals(".JPG") Or item2.Extension.Equals(".JPEG") Or item2.Extension.Equals(".PDF")) Then
 
                     TextBox1.AppendText("Name: " & item2.Name & " Creation Time: " & minutes & Environment.NewLine)
-                    TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & timeToDelete & Environment.NewLine)
+                    TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & timeToDelete.ToString & Environment.NewLine)
 
-                    If (currentMinutesTicks - minutes >= timeToDelete) Then
+                    If (currentMinutesTicks - minutes >= timeToDelete.ToString) Then
                         If (haveToMove = False) Then
                             TextBox1.AppendText("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                             System.IO.File.Delete(item & "\" & item2.Name)
@@ -43,16 +71,16 @@ Public Class Form1
                             fileWriter.Write("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                         Else
                             'item2.MoveTo(pathToMove & "\" & item2.FullName)
-                            Dim directoryPath As String = pathToMove & item2.FullName.Substring(2, item2.FullName.Length - 6)
-                            If (Not System.IO.Directory.Exists(pathToMove & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))) Then
-                                System.IO.Directory.CreateDirectory(pathToMove & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))
+                            Dim directoryPath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 6)
+                            If (Not System.IO.Directory.Exists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))) Then
+                                System.IO.Directory.CreateDirectory(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))
                             End If
-                            If (My.Computer.FileSystem.FileExists(pathToMove & item2.FullName.Substring(2, item2.FullName.Length - 2))) Then
-                                My.Computer.FileSystem.DeleteFile(pathToMove & item2.FullName.Substring(2, item2.FullName.Length - 2))
+                            If (My.Computer.FileSystem.FileExists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))) Then
+                                My.Computer.FileSystem.DeleteFile(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
                             End If
-                            Dim filePath As String = pathToMove & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 3)
-                            item2.MoveTo(pathToMove & item2.FullName.Substring(2, item2.FullName.Length - 2))
-                            TextBox1.AppendText("Moved," & item & "\" & item2.Name & "," & pathToMove & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & Environment.NewLine)
+                            Dim filePath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 3)
+                            item2.MoveTo(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
+                            TextBox1.AppendText("Moved," & item & "\" & item2.Name & "," & pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & Environment.NewLine)
                             fileWriter.Write("Moved," & item & "\" & item2.Name & "," & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                         End If
                     End If
