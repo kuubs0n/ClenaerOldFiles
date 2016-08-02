855eb5a66d5f5bd0088c873a73503dfafcf116fa
kuubs0n <jakubkrawczyk2014@gmail.com>
2016-08-02 18:39:37 +0200

    p

--- ClenaerOldFiles/Form1.vb
+++ ClenaerOldFiles/Form1.vb
@@ -31,6 +31,7 @@ Public Class Form1
         Dim pathFrom As StringBuilder = New StringBuilder(100)
         Dim pathTo As StringBuilder = New StringBuilder(100)
         Dim startupPath As String = Application.StartupPath()
+
         GetPrivateProfileString("SETTINGS", "TIME", "", timeToDelete, timeToDelete.Capacity, startupPath & "\config.ini")
         GetPrivateProfileString("SETTINGS", "FROM", "", pathFrom, pathFrom.Capacity, startupPath & "\config.ini")
         Try
@@ -39,10 +40,10 @@ Public Class Form1
         Catch
             haveToMove = False
         End Try
+
         If (pathTo.Length = 0) Then
             haveToMove = False
         End If
-
 
         Dim currentMinutesTicks As Integer = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalMinutes
         Dim fileWriter As New StreamWriter("logs.csv", True)
@@ -51,42 +52,43 @@ Public Class Form1
         TextBox1.AppendText("Interval is set on: " & timeToDelete.ToString & Environment.NewLine)
 
         Dim subfoldersList As String() = GetSubDirectories(pathFrom.ToString)
+        If (pathFrom.Length > 0 And timeToDelete.Length > 0) Then
+            For Each item In subfoldersList
+                Dim directoryInfo As New DirectoryInfo(item)
+                Dim fileList As FileInfo() = directoryInfo.GetFiles()
+                For Each item2 In fileList
+                    Dim minutes As Integer = TimeSpan.FromTicks(item2.CreationTime.Ticks).TotalMinutes
 
-        For Each item In subfoldersList
-            Dim directoryInfo As New DirectoryInfo(item)
-            Dim fileList As FileInfo() = directoryInfo.GetFiles()
-            For Each item2 In fileList
-                Dim minutes As Integer = TimeSpan.FromTicks(item2.CreationTime.Ticks).TotalMinutes
+                    If (item2.Extension.Equals(".pdf") Or item2.Extension.Equals(".jpeg") Or item2.Extension.Equals(".jpg") Or item2.Extension.Equals(".JPG") Or item2.Extension.Equals(".JPEG") Or item2.Extension.Equals(".PDF")) Then
 
-                If (item2.Extension.Equals(".pdf") Or item2.Extension.Equals(".jpeg") Or item2.Extension.Equals(".jpg") Or item2.Extension.Equals(".JPG") Or item2.Extension.Equals(".JPEG") Or item2.Extension.Equals(".PDF")) Then
+                        TextBox1.AppendText("Name: " & item2.Name & " Creation Time: " & minutes & Environment.NewLine)
+                        TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & timeToDelete.ToString & Environment.NewLine)
 
-                    TextBox1.AppendText("Name: " & item2.Name & " Creation Time: " & minutes & Environment.NewLine)
-                    TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & timeToDelete.ToString & Environment.NewLine)
+                        If (currentMinutesTicks - minutes >= timeToDelete.ToString) Then
+                            If (haveToMove = False) Then
+                                TextBox1.AppendText("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
+                                System.IO.File.Delete(item & "\" & item2.Name)
 
-                    If (currentMinutesTicks - minutes >= timeToDelete.ToString) Then
-                        If (haveToMove = False) Then
-                            TextBox1.AppendText("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
-                            System.IO.File.Delete(item & "\" & item2.Name)
-
-                            fileWriter.Write("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
-                        Else
-                            'item2.MoveTo(pathToMove & "\" & item2.FullName)
-                            Dim directoryPath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 6)
-                            If (Not System.IO.Directory.Exists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))) Then
-                                System.IO.Directory.CreateDirectory(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))
+                                fileWriter.Write("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
+                            Else
+                                'item2.MoveTo(pathToMove & "\" & item2.FullName)
+                                Dim directoryPath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 6)
+                                If (Not System.IO.Directory.Exists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))) Then
+                                    System.IO.Directory.CreateDirectory(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))
+                                End If
+                                If (My.Computer.FileSystem.FileExists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))) Then
+                                    My.Computer.FileSystem.DeleteFile(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
+                                End If
+                                Dim filePath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 3)
+                                item2.MoveTo(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
+                                TextBox1.AppendText("Moved," & item & "\" & item2.Name & "," & pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & Environment.NewLine)
+                                fileWriter.Write("Moved," & item & "\" & item2.Name & "," & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                             End If
-                            If (My.Computer.FileSystem.FileExists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))) Then
-                                My.Computer.FileSystem.DeleteFile(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
-                            End If
-                            Dim filePath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 3)
-                            item2.MoveTo(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
-                            TextBox1.AppendText("Moved," & item & "\" & item2.Name & "," & pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & Environment.NewLine)
-                            fileWriter.Write("Moved," & item & "\" & item2.Name & "," & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                         End If
                     End If
-                End If
+                Next
             Next
-        Next
+        End If
         fileWriter.Close()
         Application.Exit()
     End Sub
