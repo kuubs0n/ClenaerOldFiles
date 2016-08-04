48929eded6faa8ce0feff301cf4033132c210e80
kuubs0n <jakubkrawczyk2014@gmail.com>
2016-08-04 18:47:50 +0200

    DONE v4

--- ClenaerOldFiles/Form1.Designer.vb
+++ ClenaerOldFiles/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -33,33 +33,27 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
         Me.Label1.Location = New System.Drawing.Point(15, 15)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(0, 13)
         Me.Label1.TabIndex = 0
-
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(15, 15)
-        Me.Label2.Name = "Label1"
+        Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(0, 13)
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "TEST"
         '
-        'ImageList1
-        '
-        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
-        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
-        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
-        '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.ClenaerOldFiles.My.Resources.Resources.kozieglowy
-        Me.PictureBox1.Location = New System.Drawing.Point(556, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(545, 15)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(282, 188)
+        Me.PictureBox1.Size = New System.Drawing.Size(142, 102)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
@@ -67,20 +61,17 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(850, 550)
+        Me.ClientSize = New System.Drawing.Size(750, 275)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MinimizeBox = False
-        Me.Name = "Cleaner Old Files"
+        Me.Name = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
-    End Sub
-
-    Public Sub Form1_Activated(ByVal sender As System.Object, ByVal e As System.EventArgs)
     End Sub
 
     Public Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Shown
@@ -91,26 +82,25 @@ Partial Class Form1
         Me.Work()
     End Sub
     Public Sub PrepareGui()
-        Me.Label1.Text = "Program CleanerOldFiles napisany dla ZD Kozieglowy. Sierpień 2016" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "Autorzy: Jakub Krawczyk <jakubkrawczyk2014@gmail.com> Wojciech Pacyna <wojciech@note.pl>" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "Program kasuje lub przesuwa do innego folderu pliki starsze niż X minut" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "Dotyczy to wskazanego folderu oraz jego podfolderów" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "Jeśli nie wskazano folderu docelowego - pliki zostaną skasowane" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "Program zamyka swoje okno po 15 sekundach od zakończenia pracy" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
+        Me.Label1.Text = "Program CleanerOldFiles napisany dla ZD Kozieglowy. Sierpień 2016"
+        Me.Label1.Text += "Autorzy: Jakub Krawczyk <jakubkrawczyk2014@gmail.com> Wojciech Pacyna <wojciech@note.pl>"
+        Me.Label1.Text += "Program kasuje lub przesuwa do innego folderu pliki starsze niż X minut"
+        Me.Label1.Text += "Dotyczy to wskazanego folderu oraz jego podfolderów"
+        Me.Label1.Text += "Jeśli nie wskazano folderu docelowego - pliki zostaną skasowane"
+        Me.Label1.Text += "Program zamyka swoje okno po 15 sekundach od zakończenia pracy"
+        Me.Label1.Text += "____________________________________________________________________________________"
 
-        Me.Label1.Text += "____________________________________________________________________________________" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
+        Me.Label1.Text += "Przykładowa konfiguracja pliku config.ini poniżej:"
+        Me.Label1.Text += "katalog_zrodlowy=""C:\Skany"""
+        Me.Label1.Text += "wiek_pliku_w_minutach=120"
+        Me.Label1.Text += "opcjonalny_katalog_docelowy=""C:\stare_skany"""
 
-        Me.Label1.Text += "Przykładowa konfiguracja pliku config.ini poniżej:" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "katalog_zrodlowy=""C:\Skany""" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "wiek_pliku_w_minutach=120" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "opcjonalny_katalog_docelowy=""C:\stare_skany""" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
+        Me.Label1.Text += "____________________________________________________________________________________"
 
-        Me.Label1.Text += "____________________________________________________________________________________" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
-
-        Me.Label1.Text += "Ten program został wywołany z poniższymi parametrami" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "katalog_zrodlowy=" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "wiek_pliku_w_minutach=" & Environment.NewLine & Environment.NewLine
-        Me.Label1.Text += "opcjonalny_katalog_docelowy=" & Environment.NewLine & Environment.NewLine
+        Me.Label1.Text += "Ten program został wywołany z poniższymi parametrami"
+        Me.Label1.Text += "katalog_zrodlowy="
+        Me.Label1.Text += "wiek_pliku_w_minutach="
+        Me.Label1.Text += "opcjonalny_katalog_docelowy="
     End Sub
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
--- ClenaerOldFiles/Form1.vb
+++ ClenaerOldFiles/Form1.vb
@@ -13,28 +13,22 @@ Public Class Form1
             ByVal nSize As Integer,
             ByVal lpFileName As String) As Integer
     Sub PrepareGui(ByVal pathFrom As String, ByVal time As String, ByVal pathTo As String)
-        Label1.Text = "Program CleanerOldFiles napisany dla ZD Kozieglowy. Sierpień 2016" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "Autorzy: Jakub Krawczyk <jakubkrawczyk2014@gmail.com> Wojciech Pacyna <wojciech@note.pl>" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "Program kasuje lub przesuwa do innego folderu pliki starsze niż X minut" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "Dotyczy to wskazanego folderu oraz jego podfolderów" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "Jeśli nie wskazano folderu docelowego - pliki zostaną skasowane" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "Program zamyka swoje okno po 15 sekundach od zakończenia pracy" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
+        Label1.Text = "Program CleanerOldFiles napisany dla ZD Kozieglowy. Sierpień 2016" & Environment.NewLine
+        Label1.Text += "Autorzy: Jakub Krawczyk <jakubkrawczyk2014@gmail.com> Wojciech Pacyna <wojciech@note.pl>" & Environment.NewLine
+        Label1.Text += "Program kasuje lub przesuwa do innego folderu pliki starsze niż X minut" & Environment.NewLine
+        Label1.Text += "Dotyczy to wskazanego folderu oraz jego podfolderów" & Environment.NewLine
+        Label1.Text += "Jeśli nie wskazano folderu docelowego - pliki zostaną skasowane" & Environment.NewLine
+        Label1.Text += "Program zamyka swoje okno po 15 sekundach od zakończenia pracy" & Environment.NewLine & Environment.NewLine
 
-        Label1.Text += "____________________________________________________________________________________" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
+        Label1.Text += Environment.NewLine & "Przykładowa konfiguracja pliku config.ini poniżej:" & Environment.NewLine
+        Label1.Text += "katalog_zrodlowy=""C:\Skany""" & Environment.NewLine
+        Label1.Text += "wiek_pliku_w_minutach=""120""" & Environment.NewLine
+        Label1.Text += "opcjonalny_katalog_docelowy=""C:\stare_skany""" & Environment.NewLine & Environment.NewLine
 
-        Label1.Text += "Przykładowa konfiguracja pliku config.ini poniżej:" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "katalog_zrodlowy=""C:\Skany""" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "wiek_pliku_w_minutach=""120""" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "opcjonalny_katalog_docelowy=""C:\stare_skany""" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
-
-        Label1.Text += "____________________________________________________________________________________" & Environment.NewLine & Environment.NewLine & Environment.NewLine & Environment.NewLine
-
-        Label1.Text += "Ten program został wywołany z poniższymi parametrami" & Environment.NewLine & Environment.NewLine
-        Label1.Text += "katalog_zrodlowy=" & pathFrom & Environment.NewLine & Environment.NewLine
-        Label1.Text += "wiek_pliku_w_minutach=" & time & Environment.NewLine & Environment.NewLine
-        Label1.Text += "opcjonalny_katalog_docelowy=" & pathTo & Environment.NewLine & Environment.NewLine
-
-
+        Label1.Text += Environment.NewLine & "Ten program został wywołany z poniższymi parametrami" & Environment.NewLine
+        Label1.Text += "katalog_zrodlowy=" & pathFrom & Environment.NewLine
+        Label1.Text += "wiek_pliku_w_minutach=" & time & Environment.NewLine
+        Label1.Text += "opcjonalny_katalog_docelowy=" & pathTo & Environment.NewLine
 
     End Sub
     Sub Work()
@@ -75,10 +69,41 @@ Public Class Form1
         Dim currentMinutesTicks As Integer = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalMinutes
         Dim fileWriter As New StreamWriter("logs.csv", True)
 
-        'TextBox1.AppendText("Current time is: " & currentMinutesTicks & Environment.NewLine)
-        'TextBox1.AppendText("Interval is set on: " & timeToDelete.ToString & Environment.NewLine)
         If (pathFrom.Length > 0 And timeToDelete.Length > 0) Then
             If (System.IO.Directory.Exists(pathFrom.ToString)) Then
+                Dim directoryInfoMain As New DirectoryInfo(pathFrom.ToString)
+                Dim fileListMain As FileInfo() = directoryInfoMain.GetFiles()
+
+                For Each item0 In fileListMain
+                    Dim minutes As Integer = TimeSpan.FromTicks(item0.CreationTime.Ticks).TotalMinutes
+
+                    If (item0.Extension.Equals(".pdf") Or item0.Extension.Equals(".jpeg") Or item0.Extension.Equals(".jpg") Or item0.Extension.Equals(".PDF") _
+                        Or item0.Extension.Equals(".JPEG") Or item0.Extension.Equals(".JPG")) Then
+
+                        Dim time As Double
+                        Double.TryParse(timeToDelete.ToString, time)
+                        If (currentMinutesTicks - minutes >= time) Then
+                            If (haveToMove = False) Then
+                                System.IO.File.Delete(pathFrom.ToString & "\" & item0.Name)
+                                fileWriter.Write("Deleted," & pathFrom.ToString & "\" & item0.Name & "," & item0.CreationTime & "," & DateTime.Now & Environment.NewLine)
+                            Else
+                                Dim directoryPath As String = pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - 6)
+                                If (Not System.IO.Directory.Exists(pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - item0.Name.Length - 2))) Then
+                                    System.IO.Directory.CreateDirectory(pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - item0.Name.Length - 2))
+                                End If
+                                If (My.Computer.FileSystem.FileExists(pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - 2))) Then
+                                    My.Computer.FileSystem.DeleteFile(pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - 2))
+                                End If
+                                Dim filePath As String = pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - item0.Name.Length - 3)
+                                item0.MoveTo(pathTo.ToString & item0.FullName.Substring(2, item0.FullName.Length - 2))
+                                fileWriter.Write("Moved," & pathFrom.ToString & "\" & item0.Name & "," & item0.FullName.Substring(2, item0.FullName.Length - 2) _
+                                & "," & item0.CreationTime.ToString & "," & DateTime.Now.ToString & Environment.NewLine)
+                            End If
+                        End If
+                    End If
+                Next
+
+
                 Dim subfoldersList As String() = GetSubDirectories(pathFrom.ToString)
                 For Each item In subfoldersList
                     Dim directoryInfo As New DirectoryInfo(item)
@@ -88,15 +113,13 @@ Public Class Form1
 
                         If (item2.Extension.Equals(".pdf") Or item2.Extension.Equals(".jpeg") Or item2.Extension.Equals(".jpg") Or item2.Extension.Equals(".JPG") Or item2.Extension.Equals(".JPEG") Or item2.Extension.Equals(".PDF")) Then
 
-                            'TextBox1.AppendText("Name: " & item2.Name & " Creation Time: " & minutes & Environment.NewLine)
-                            'TextBox1.AppendText("Checking: " & currentMinutesTicks & " " & minutes & " " & timeToDelete.ToString & Environment.NewLine)
-
-                            If (currentMinutesTicks - minutes >= timeToDelete.ToString) Then
+                            Dim time As Double
+                            Double.TryParse(timeToDelete.ToString, time)
+                            If (currentMinutesTicks - minutes >= time) Then
                                 If (haveToMove = False) Then
-                                    'TextBox1.AppendText("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
                                     System.IO.File.Delete(item & "\" & item2.Name)
 
-                                    fileWriter.Write("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
+                                    fileWriter.Write("Deleted," & item & "\" & item2.Name & "," & item2.CreationTime.ToString & "," & DateTime.Now.ToString & Environment.NewLine)
                                 Else
                                     Dim directoryPath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 6)
                                     If (Not System.IO.Directory.Exists(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 2))) Then
@@ -107,8 +130,7 @@ Public Class Form1
                                     End If
                                     Dim filePath As String = pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - item2.Name.Length - 3)
                                     item2.MoveTo(pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2))
-                                    'TextBox1.AppendText("Moved," & item & "\" & item2.Name & "," & pathTo.ToString & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & Environment.NewLine)
-                                    fileWriter.Write("Moved," & item & "\" & item2.Name & "," & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & item2.CreationTime & "," & DateTime.Now & Environment.NewLine)
+                                    fileWriter.Write("Moved," & item & "\" & item2.Name & "," & item2.FullName.Substring(2, item2.FullName.Length - 2) & "," & item2.CreationTime.ToString & "," & DateTime.Now.ToString & Environment.NewLine)
                                 End If
                             End If
                         End If
@@ -136,9 +158,9 @@ Public Class Form1
             For Each s As String In GetSubDirectories(subfolder.FullName)
                 Array.Resize(path, path.Length + 1)
                 path(path.Length - 1) = s
-                'TextBox1.Text = TextBox1.Text + s + Environment.NewLine
             Next
         Next
         Return path
     End Function
+
 End Class
